df6f81fe0d82cf652d64565ac17d37daa432ad5d
pba <alassanebv23@gmail.com>
2023-09-13 16:23:33 +0000

    start

--- push.sh
+++ push.sh
@@ -2,4 +2,4 @@ git add .
 read -r -p "Your commit message: " message
 git commit -m "$message"
 git remote add origin https://learn.zone01dakar.sn/git/pba/atm-management-system.git
-git push
+git push -u origin master
