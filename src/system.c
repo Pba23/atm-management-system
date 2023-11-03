7d91fa58a5f0c6883cec6419a5ace338916c1a2d
Papa Alassane  Ba <pba@noreply.learn.zone01dakar.sn>
2023-11-03 15:21:09 +0000

    Upload files to 'src'

--- src/system.c
+++ src/system.c
@@ -6,8 +6,8 @@ int getAccountFromFile(FILE *ptr, char name[50], struct Record *r)
 {
     return fscanf(ptr, "%d %d %s %d %d/%d/%d %s %d %lf %s",
                   &r->id,
-		  &r->userId,
-		  name,
+                  &r->userId,
+                  name,
                   &r->accountNbr,
                   &r->deposit.month,
                   &r->deposit.day,
@@ -21,9 +21,9 @@ int getAccountFromFile(FILE *ptr, char name[50], struct Record *r)
 void saveAccountToFile(FILE *ptr, struct User u, struct Record r)
 {
     fprintf(ptr, "%d %d %s %d %d/%d/%d %s %d %.2lf %s\n\n",
-            &r->id,
-	    &u->id
-	    &u->name,
+            r.id,
+            u.id,
+            u.name,
             r.accountNbr,
             r.deposit.month,
             r.deposit.day,
@@ -32,6 +32,35 @@ void saveAccountToFile(FILE *ptr, struct User u, struct Record r)
             r.phone,
             r.amount,
             r.accountType);
+}
+void stayOrQuit(struct User u)
+{
+    int choice;
+    do
+    {
+        printf("\n\t\tEnter 1 to return to the main menu, or 2 to exit: ");
+        if (scanf("%d", &choice) != 1)
+        {
+            printf("Invalid input. Please enter a valid option.\n");
+            while (getchar() != '\n')
+                ;
+        }
+        else
+        {
+            switch (choice)
+            {
+            case 1:
+                mainMenu(u);
+                break;
+            case 2:
+                exit(0);
+                break;
+            default:
+                printf("Invalid option. Please enter a valid option.\n");
+                break;
+            }
+        }
+    } while (1);
 }
 
 void stayOrReturn(int notGood, void f(struct User u), struct User u)
@@ -76,7 +105,7 @@ void stayOrReturn(int notGood, void f(struct User u), struct User u)
 void success(struct User u)
 {
     int option;
-    printf("\n✔ Success!\n\n");
+    printf("\n✔ Success !\n\n");
 invalid:
     printf("Enter 1 to go to the main menu and 0 to exit!\n");
     scanf("%d", &option);
@@ -95,10 +124,46 @@ invalid:
         goto invalid;
     }
 }
+void getAllRecords(struct Record AllRecords[], int *numRecords)
+{
+    FILE *fp;
+    char filename[] = "./data/records.txt"; // Nom du fichier
+    int count = 0;                          // Compteur du nombre d'utilisateurs
 
+    // Ouvrir le fichier en mode lecture
+    if ((fp = fopen(filename, "r")) == NULL)
+    {
+        printf("Error oppenning file : %s\n", filename);
+        exit(1);
+    }
+
+    // Lire les données du fichier
+    while (fscanf(fp, "%d %d %99s %d %d/%d/%d %99s %d %lf %9s",
+                  &AllRecords[count].id,
+                  &AllRecords[count].userId,
+                  AllRecords[count].name,
+                  &AllRecords[count].accountNbr,
+                  &AllRecords[count].deposit.day,
+                  &AllRecords[count].deposit.month,
+                  &AllRecords[count].deposit.year,
+                  AllRecords[count].country,
+                  &AllRecords[count].phone,
+                  &AllRecords[count].amount,
+                  AllRecords[count].accountType) == 11)
+    {
+        count++;
+    }
+    // Fermer le fichier
+    fclose(fp);
+
+    // Mettre à jour le nombre d'utilisateurs
+    *numRecords = count;
+}
 void createNewAcc(struct User u)
 {
     struct Record r;
+    struct Record AllRecords[100];
+    int num;
     struct Record cr;
     char userName[50];
     FILE *pf = fopen(RECORDS, "a+");
@@ -128,7 +193,8 @@ noAccount:
     scanf("%lf", &r.amount);
     printf("\nChoose the type of account:\n\t-> saving\n\t-> current\n\t-> fixed01(for 1 year)\n\t-> fixed02(for 2 years)\n\t-> fixed03(for 3 years)\n\n\tEnter your choice:");
     scanf("%s", r.accountType);
-
+    getAllRecords(AllRecords, &num);
+    r.id = num;
     saveAccountToFile(pf, u, r);
 
     fclose(pf);
@@ -162,4 +228,249 @@ void checkAllAccounts(struct User u)
     }
     fclose(pf);
     success(u);
+}
+
+void changeCountry(struct Record AllRecords[], int numRecords, char country[100], struct User u, int accountNumber)
+{
+    FILE *fp;
+    char filename[] = "./data/records.txt";
+
+    if ((fp = fopen(filename, "w")) == NULL)
+    {
+        printf("Error oppenning file : %s\n", filename);
+        return;
+    }
+
+    for (int i = 0; i < numRecords; i++)
+    {
+        if (strcmp(AllRecords[i].name, u.name) == 0 && AllRecords[i].id == accountNumber)
+        {
+            fprintf(fp, "%d %d %s %d %d/%d/%d %s %d %.2lf %s\n",
+                    AllRecords[i].id, AllRecords[i].userId, AllRecords[i].name, AllRecords[i].accountNbr,
+                    AllRecords[i].deposit.day, AllRecords[i].deposit.month, AllRecords[i].deposit.year,
+                    country, AllRecords[i].phone, AllRecords[i].amount, AllRecords[i].accountType);
+        }
+        else
+        {
+            fprintf(fp, "%d %d %s %d %d/%d/%d %s %d %.2lf %s\n",
+                    AllRecords[i].id, AllRecords[i].userId, AllRecords[i].name, AllRecords[i].accountNbr,
+                    AllRecords[i].deposit.day, AllRecords[i].deposit.month, AllRecords[i].deposit.year,
+                    AllRecords[i].country, AllRecords[i].phone, AllRecords[i].amount, AllRecords[i].accountType);
+        }
+    }
+
+    fclose(fp);
+}
+void changeNumber(struct Record AllRecords[], int numRecords, int number, struct User u, int accountNumber)
+{
+    FILE *fp;
+    char filename[] = "./data/records.txt";
+
+    if ((fp = fopen(filename, "w")) == NULL)
+    {
+        printf("Error oppenning file : %s\n", filename);
+        return;
+    }
+
+    for (int i = 0; i < numRecords; i++)
+    {
+        if (strcmp(AllRecords[i].name, u.name) == 0 && AllRecords[i].accountNbr == accountNumber)
+        {
+            fprintf(fp, "%d %d %s %d %d/%d/%d %s %d %.2lf %s\n",
+                    AllRecords[i].id, AllRecords[i].userId, AllRecords[i].name, AllRecords[i].accountNbr,
+                    AllRecords[i].deposit.day, AllRecords[i].deposit.month, AllRecords[i].deposit.year,
+                    AllRecords[i].country, number, AllRecords[i].amount, AllRecords[i].accountType);
+        }
+        else
+        {
+            fprintf(fp, "%d %d %s %d %d/%d/%d %s %d %.2lf %s\n",
+                    AllRecords[i].id, AllRecords[i].userId, AllRecords[i].name, AllRecords[i].accountNbr,
+                    AllRecords[i].deposit.day, AllRecords[i].deposit.month, AllRecords[i].deposit.year,
+                    AllRecords[i].country, AllRecords[i].phone, AllRecords[i].amount, AllRecords[i].accountType);
+        }
+    }
+
+    fclose(fp);
+}
+
+
+void interestMessage(struct Record r)
+{
+    float value;
+    if (strcmp(r.accountType, "current") == 0)
+    {
+        printf("You will not get interests because the account is of type current");
+    }
+    else
+    {
+        float interest;
+        if (strcmp(r.accountType, "saving") == 0)
+        {
+            interest = 0.07;
+            value = r.amount * interest / 12;
+            printf("You will get $%.2f as interest on day %d of every month", value, r.deposit.day);
+        }
+        else if (strcmp(r.accountType, "fixed01") == 0)
+        {
+            interest = 0.04;
+            value = r.amount * interest;
+            printf("You will get $%.2f as interest on %d/%d/%d of every month", value, r.deposit.day, r.deposit.month, r.deposit.year + 1);
+        }
+        else if (strcmp(r.accountType, "fixed02") == 0)
+        {
+            interest = 0.05;
+            value = r.amount * interest * 2;
+            printf("You will get $%.2f as interest on %d/%d/%d of every month", value, r.deposit.day, r.deposit.month, r.deposit.year + 2);
+        }
+        else if (strcmp(r.accountType, "fixed03") == 0)
+        {
+            interest = 0.08;
+            value = r.amount * interest * 3;
+            printf("You will get $%.2f as interest on %d/%d/%d of every month", value, r.deposit.day, r.deposit.month, r.deposit.year + 3);
+        }
+    }
+}
+void countDetail(struct User u)
+{
+    char userName[100];
+    FILE *pf = fopen(RECORDS, "r");
+    struct Record r;
+    int id;
+    int count = 0;
+    system("clear");
+    printf("\t\t====== Account datail =====\n\n");
+    printf("\n\n\n\n\n\t\t\t\tEnter your account number:");
+    int counter;
+    scanf("%d", &id);
+    while (getAccountFromFile(pf, userName, &r))
+    {
+        if (strcmp(userName, u.name) == 0 && id == r.accountNbr)
+        {
+            printf("Account number: %d\n", r.accountNbr);
+            printf("Deposit Date: %d/%d/%d\n", r.deposit.day, r.deposit.month, r.deposit.year);
+            printf("Country: %s\n", r.country);
+            printf("Phone number: %d\n", r.phone);
+            printf("Amount deposited: %.2f\n", r.amount);
+            printf("Type Of Account : %s\n", r.accountType);
+            interestMessage(r);
+            stayOrQuit(u);
+            count++;
+        }
+    }
+    if (count == 0)
+    {
+        printf("This account number does'nt exists or is'nt yours");
+        stayOrQuit(u);
+    }
+}
+
+// void (struct User u)
+// {
+//     int choice;
+//     // Code pour initialiser les données nécessaires (comme les comptes) ici
+//     char op[10];
+//     do
+//     {
+//         system("clear");
+//         printf("\nTransaction Menu\n");
+//         printf("[1]. Make a deposit\n");
+//         printf("[2]. Make a withdrawal\n");
+//         printf("[3]. Return to Main Menu\n");
+//         printf("[4]. Exit\n");
+//         printf("Enter your choice: ");
+
+//         if (scanf("%d", &choice) != 1)
+//         {
+//             printf("Invalid input. Please enter a valid option.\n");
+//             while (getchar() != '\n')
+//                 ; // Vide le tampon d'entrée
+//         }
+//         else
+//         {
+//             switch (choice)
+//             {
+//             case 1:
+
+//                 strcpy(op, "deposit");
+//                 operation(op, u);
+//                 break;
+//             case 2:
+
+//                 strcpy(op, "withdraw");
+//                 operation(op, u);
+//                 break;
+//             case 3:
+//                 mainMenu(u);
+//                 return;
+//             case 4:
+//                 exit(0);
+//                 break;
+//             default:
+//                 printf("Invalid option. Please enter a valid option.\n");
+//                 break;
+//             }
+//             break;
+//         }
+//     } while (1); // Répétez jusqu'à ce que l'utilisateur entre une option valide
+// }
+void removeAccount(struct User u)
+{
+    int numRecords;
+    struct Record AllRecords[100];
+    int accountID;
+    int count = 0;
+    getAllRecords(AllRecords, &numRecords);
+
+    system("clear");
+    printf("\n\n\t\tEnter the id of the count to remove : ");
+
+    if (scanf("%d", &accountID) != 1)
+    {
+        printf("Invalid input. Please enter a valid option.\n");
+        while (getchar() != '\n')
+            ; // Vide le tampon d'entrée
+    }
+    for (int i = 0; i < numRecords; i++)
+    {
+        if (strcmp(AllRecords[i].name, u.name) == 0 && AllRecords[i].accountNbr == accountID)
+        {
+            count++;
+        }
+    }
+
+    if (count == 0)
+    {
+        printf("\n\n\t\tThis account number does'nt exist or is'nt yours\n");
+        stayOrQuit(u);
+    }
+    else
+    {
+        FILE *fp;
+        char filename[] = "./data/records.txt";
+
+        if ((fp = fopen(filename, "w")) == NULL)
+        {
+            printf("Error oppenning file : %s\n", filename);
+            return;
+        }
+        for (int i = 0; i < numRecords; i++)
+        {
+            if (strcmp(AllRecords[i].name, u.name) == 0 && AllRecords[i].accountNbr == accountID)
+            {
+                count++;
+            }
+            else
+            {
+                fprintf(fp, "%d %d %s %d %d/%d/%d %s %d %.2lf %s\n",
+                        AllRecords[i].id, AllRecords[i].userId, AllRecords[i].name, AllRecords[i].accountNbr,
+                        AllRecords[i].deposit.day, AllRecords[i].deposit.month, AllRecords[i].deposit.year,
+                        AllRecords[i].country, AllRecords[i].phone, AllRecords[i].amount, AllRecords[i].accountType);
+            }
+        }
+        fclose(fp);
+    }
+    {
+        success(u);
+        stayOrQuit(u);
+    }
 }
